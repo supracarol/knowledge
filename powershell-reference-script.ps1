8495651eb8fd1aef142db9561a49ca27fd883d11
supracarol <69781678+supracarol@users.noreply.github.com>
2022-04-16 21:40:08 -0700

    Create powershell-reference-script.ps1

---
+++ powershell-reference-script.ps1
@@ -0,0 +1,10 @@
+# Basic reference for powershell script
+# Running:
+#     powershell.exe -ExecutionPolicy bybpass -file "C:\path\to\powershell-reference-script.ps1"
+
+# Emulate 'set -e'
+Set-StrictMode -Version latest
+$ErrorActionPreference = "Stop"
+
+# Set working directory to directory of this script
+Set-Location -Path "$PSScriptRoot"
